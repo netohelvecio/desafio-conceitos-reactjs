43760618b33ee0110c3c18d1176283aee34377c0
netohelvecio <helvecioneto77@gmail.com>
2020-06-15 21:05:54 -0300

    feat: listing repositories

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories')
+      .then(response => {
+        console.log(response.data);
+        setRepositories(response.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
+
   async function handleAddRepository() {
     // TODO
   }
@@ -12,18 +27,38 @@ function App() {
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+    <div className="container">
+      <h1>Criar repósitorio</h1>
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+      <form>
+        <input type="text" placeholder="URL do repósitorio"/>
+
+        <input type="text" placeholder="Titúlo do repósitorio"/>
+
+        <span>Separar tecnlogias por virgúlas</span>
+        <input type="text" placeholder="Teconlogias utilizadas"/>
+
+        <button type="submit" onClick={handleAddRepository}>Adicionar</button>
+      </form>
+
+      <ul data-testid="repository-list">
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            <span>{repository.title}</span>
+
+            <p>{repository.techs.join(', ')}.</p>
+
+            <div>
+              <a href={repository.url}>Link para repósitorio</a>
+
+              <button onClick={() => handleRemoveRepository(1)}>
+              Remover
+              </button>
+            </div>
+          </li>
+        ))}
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
